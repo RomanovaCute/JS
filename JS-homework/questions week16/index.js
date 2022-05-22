9c3f57c325863a4581d6db11a0cbc73e265c82ae
RomanovaCute <romanova17romanova@yandex.ru>
2022-05-22 23:13:06 +0500

    task 3 week 16

---
+++ JS-homework/questions week16/index.js
@@ -0,0 +1,30 @@
+//Вопрос №1
+let array = ['js', 'css', 'html'];
+console.log(array[1]);
+
+//Вопрос №2
+let array1 = [0, 1, false, 2, undefined, '', 3, null];
+let filteredArray = array1.filter(el=>el !== false && el !== undefined && el !== '' && el !==null && el !== 0);
+console.log(filteredArray);
+
+//Вопрос №3
+let arrays = [
+    [1,2], 
+    [1,2,3], 
+    [1,2,3,4],
+]
+
+let findIt2 = arrays.findIndex(element => element.length>3);
+console.log(findIt2);
+
+
+
+
+// let findIt = arrays.findIndex((element)=> {
+//     if(element.length>3){
+//         return true;
+//     }
+// })
+// console.log(findIt);
+
+
