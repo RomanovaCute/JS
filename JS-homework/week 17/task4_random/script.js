ff03dafa53f3624d70f18dae66d9e3554af2d52a
RomanovaCute <romanova17romanova@yandex.ru>
2022-06-07 16:36:42 +0500

    week 17

--- JS-homework/week 17/task4_random/script.js
+++ JS-homework/week 17/task4_random/script.js
@@ -1,32 +1,40 @@
-let numRandom;
-let nums = [];
-let min,
+function random(){
+    // Для того, чтобы не писать отдельно "Очистку массива", объявляем переменные сразу в фукнции
+    let nums = [];
+    let min,
     max,
     average,
     sum = 0, //Изначально присваиваем 0, чтобы все последующие элементы массива прибавлялись к нему, если не задать будет выводить NaN при первом нажатии на кнопку
     mult = 1; //Аналогично
-
-function random(){
     
-    //Выводится массив рандомных чисел
-    for (let i = 0; i<10; i++){
-        numRandom = Math.round((Math.random()*(20) - 10));
-        nums.push(numRandom);
+    //Выводится массив рандомных чисел - происходит рандом и сразу числа добавляются друг за другом
+    for (let i = 0; i < 10; i++){
+        nums.push(Math.round(Math.random()*20 - 10));
     }
 
-    min = Math.min.apply(Math, nums); //Минимальное значение из массива чисел
-    max = Math.max.apply(Math, nums); //Максимальное число из массива
+    // ... сделают так, что [1,2,3] скобки пропадут, и останутся чисто параметры 1,2,3
+    min = Math.min(...nums); //Минимальное значение из массива чисел
+    max = Math.max(...nums); //Максимальное число из массива
 
-    for(let index = 0; index<nums.length; index++){
-        sum += Number(nums[index]); //Находим сумму элементов массива
-    }
+    /**
+     * @description Сложение и умнржение полученных чисел: 
+     * @param {number} a - первый элемент (текущее значение)
+     * @param {number} b - второй элемент (предыдущее значение)
+     */
+    sum = nums.reduce((a,b) => a + b);
+    mult = nums.reduce((a,b) => a * b);
 
-    average = sum/10; //Среднее арифметическое
+    average = sum/nums.length; //Среднее арифметическое
 
+    // Сложение
+    // for(let index = 0; index<nums.length; index++){
+    //     sum += Number(nums[index]);
+    // }
 
-    for(let index1 = 0; index1<nums.length; index1++){
-        mult *= Number(nums[index1]);
-    }
+    // Умножение
+    // for(let index1 = 0; index1<nums.length; index1++){
+    //     mult *= Number(nums[index1]);
+    // }
 
     document.querySelector('.arr').innerHTML = `Сгенерировали: ${nums.join(', ')}`;
     document.querySelector('.min').innerHTML = `Минимальное: ${min}`;
@@ -34,15 +42,5 @@ function random(){
     document.querySelector('.average').innerHTML = `Среднее арифметическое: ${average}`;
     document.querySelector('.sum').innerHTML = `Сумма чисел: ${sum}`;
     document.querySelector('.mult').innerHTML = `Произведение чисел: ${mult}`;
-
-    //Очистка массива
-    for (let index2 = 0; index2 < 10; index2++) {
-        nums.pop(nums[index2]);
-    }
-
-    // Очистка значения суммы и произведения
-    sum = 0;
-    mult = 1;
-}
 
 button.addEventListener('click', random);
