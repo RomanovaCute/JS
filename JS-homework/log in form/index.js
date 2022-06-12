8806b7e13c72a7d68b7dd34c2db893fefd083d6a
RomanovaCute <romanova17romanova@yandex.ru>
2022-06-12 18:05:13 +0500

    week 18

--- JS-homework/log in form/index.js
+++ JS-homework/log in form/index.js
@@ -1,58 +1,124 @@
-function checkIt(){
-    let check = true;
-    let name = document.getElementById('name');
-    let surname = document.getElementById('surname');
-    let country = document.getElementById('country');
-    let phone = document.getElementById('phone');
-    let email = document.getElementById('email');
-    let password = document.getElementById('password');
-    // const beginWithoutDigit = /^\D.*$/; /*переменная, обозначающая ввод цифр*/
+// function checkIt(){
+    // let check = true;
+    let name = document.querySelector('#name').value;
+    let surname = document.querySelector('#surname').value;
+    let country = document.querySelector('#country').value;
+    let phone = document.querySelector('#phone').value;
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
 
-    document.getElementById('errorMessage1').innerHTML = "";
-    if (name.value == ''){
-        document.getElementById('errorMessage1').innerHTML += 'Ваше имя не заполнено!';
-        check = false;
-    }
-
-    document.getElementById('errorMessage2').innerHTML = "";
-    if (surname.value == ''){
-        document.getElementById('errorMessage2').innerHTML += 'Ваша фамилия не заполнена!';
-        check = false;
+    function validateName(name){
+        let nameFormat = /^([А-Я]{1}[а-яё]{1,30}|[A-Z]{1}[a-z]{1,30})$/gm;
+        if (nameFormat.test(name)){
+            return true;
+        } else {
+            document.querySelector('#errorMessage1').innerHTML += 'Ваше имя заполнено неверно';
+            return false;
+        }
     }
 
-    document.getElementById('errorMessage3').innerHTML = "";
-    if (country.value == ''){
-        document.getElementById('errorMessage3').innerHTML += 'Ваша страна не выбрана!';
-        check = false;
+    function validateSurname(surname){
+        let surnameFormat = /^([А-Я]{1}[а-яё]{1,30}|[A-Z]{1}[a-z]{1,30})$/gm;
+        if (surnameFormat.test(surname)){
+            return true;
+        } else {
+            document.querySelector('#errorMessage2').innerHTML += 'Ваша фамилия заполнена неверно';
+            return false;
+        }
     }
 
-    document.getElementById('errorMessage4').innerHTML = "";
-    if (phone.value == ''){
-        document.getElementById('errorMessage4').innerHTML += 'Ваш номер телефона не заполнен!';
-        check = false;
-    }
-
-    document.getElementById('errorMessage5').innerHTML = "";
-    if (email.value == ''){
-        document.getElementById('errorMessage5').innerHTML += 'Ваше e-mail не заполнен!';
-        check = false;
-    }
-
-    document.getElementById('errorMessage6').innerHTML = "";
-    if (password.value == ''){
-        document.getElementById('errorMessage6').innerHTML += 'Введите пароль!';
-        check = false;
-    }
-    if(password.value.length < 8){
-        document.getElementById('errorMessage6').innerHTML += 'Ваш пароль слишком короткий!'
-        check = false;
-    }
-    // if(beginWithoutDigit.test(password)){
-    //     document.getElementById('errorMessage6').innerHTML += 'Пароль не должен начинаться с цифр!'
+    // function vidateCountry(country){
+    //     if (country.options[country.selectedIndex].value == 'selected'){
+    //         document.querySelector('#errorMessage3').innerHTML += 'Ваша страна не выбрана!';
+    //     }
     // }
 
-    if(check === true){
-    alert ('Региcтрация прошла успешно!');
+    function validatePhoneNumber(phone){
+        let phoneFormat = /^(\+)?((\d{2,3}) ?\d|\d)(([ -]?\d)|( ?(\d{2,3}) ?)){5,12}\d$/gm;
+        if (phoneFormat.test(phone)){
+            return true;
+        } else {
+            document.querySelector('#errorMessage4').innerHTML += 'Ваш номер телефона заполнен не верно';
+            return false;
+        }
     }
-}
-button.addEventListener('click',checkIt);
+
+    function validateEmail(email){
+        let emailFormat = /^[a-zA-Z0-9_\.]{4,16}@[a-z]{3,10}\.[a-z]{2,3}$/gm;
+        if (emailFormat.test(email)){
+            return true;
+        } else {
+            document.querySelector('#errorMessage5').innerHTML += 'Ваш email заполнен не верно';
+            return false;
+        }
+    }
+
+    function validatePassword(password){
+        let passwordFormat = /^(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9!@#$%^&*a-zA-Z]{6,}$/gm;
+        if (passwordFormat.test(password)){
+            return true;
+        } else {
+            document.querySelector('#errorMessage6').innerHTML += 'Ваш пароль не надежный!';
+            return false;
+        }
+    }
+
+button.addEventListener('click', function(){
+    validateName(),
+    validateSurname(),
+    validatePhoneNumber(),
+    validateEmail(),
+    validatePassword();
+});    
+
+
+
+//Старая версия
+
+//     document.querySelector('errorMessage1').innerHTML = "";
+//     if (name.value == ''){
+//         document.querySelector('errorMessage1').innerHTML += 'Ваше имя не заполнено!';
+//         check = false;
+//     }
+
+//     document.querySelector('errorMessage2').innerHTML = "";
+//     if (surname.value == ''){
+//         document.querySelector('errorMessage2').innerHTML += 'Ваша фамилия не заполнена!';
+//         check = false;
+//     }
+
+//     document.querySelector('errorMessage3').innerHTML = "";
+//     if (country.value == ''){
+//         document.querySelector('errorMessage3').innerHTML += 'Ваша страна не выбрана!';
+//         check = false;
+//     }
+
+//     document.querySelector('errorMessage4').innerHTML = "";
+//     if (phone.value == ''){
+//         document.querySelector('errorMessage4').innerHTML += 'Ваш номер телефона не заполнен!';
+//         check = false;
+//     }
+
+//     document.querySelector('errorMessage5').innerHTML = "";
+//     if (email.value == ''){
+//         document.querySelector('errorMessage5').innerHTML += 'Ваше e-mail не заполнен!';
+//         check = false;
+//     }
+
+//     document.querySelector('errorMessage6').innerHTML = "";
+//     if (password.value == ''){
+//         document.querySelector('errorMessage6').innerHTML += 'Введите пароль!';
+//         check = false;
+//     }
+//     if(password.value.length < 8){
+//         document.querySelector('errorMessage6').innerHTML += 'Ваш пароль слишком короткий!'
+//         check = false;
+//     }
+//     // if(beginWithoutDigit.test(password)){
+//     //     document.querySelector('errorMessage6').innerHTML += 'Пароль не должен начинаться с цифр!'
+//     // }
+
+//     if(check === true){
+//     alert ('Региcтрация прошла успешно!');
+//     }
+// }
--- JS-homework/week 17/task2_spamfilter/index.js
+++ JS-homework/week 17/task2_spamfilter/index.js
@@ -1,22 +1,63 @@
-const list = document.querySelector('#commentsList');
+const input = document.querySelector('input'),
+    button = document.querySelector('#button'),
+    list = document.querySelector('#commentsList');
 
-function censor (){
-    let comment = document.querySelector('input[name="newComment"]').value
-        .replace(/viagra/gi, '***')
-        .replace(/xxx/gi, '***');
+document.addEventListener('DOMContentLoaded', () => {
+    const comments = getData();
+    
+    comments.map(comment => {
+        const box = document.createElement('div');
+        box.innerHTML = comment;
+
+        list.append(box);
+    });
+});
+
+
+function setData(value){
+    localStorage.setItem('comments', JSON.stringify(comment.push(value)));
+}
+
+function getData(){
+    return JSON.parse(localStorage.getItem('comments')) || [];
+}
+
+button.addEventListener('click', postComment)
+
+function postComment(){
+    input.value;
+    const comment = getData();
+    
+    setData(input.value);
+}
+
+
+
+
+
+
+
+
+
+
+
+// function censor (){
+//     let comment = document.querySelector('input[name="newComment"]').value
+//         .replace(/viagra/gi, '***')
+//         .replace(/xxx/gi, '***');
 
     // escape-последовательности применимы тольк внутри строки, например,
     // 'строка1\n строка2\n строка3\n'
     // но при конкатинации разных отедльных констант escape-последовательность не сработает
     
-    const newListItem = document.createElement('div'); //создает <div></div>
-    newListItem.innerHTML = comment; //добавлет коммент. <div>текст</div>
+    // const newListItem = document.createElement('div'); //создает <div></div>
+    // newListItem.innerHTML = comment; //добавлет коммент. <div>текст</div>
 
     // append - добавление наших созданных <div>текст</div> в конец
     // prepend - добавление в начало списка
-    list.append(newListItem);
+   
 
-    document.querySelector('input[name="newComment"]').value = '';
-}
+//     document.querySelector('input[name="newComment"]').value = '';
+// }
 
-document.querySelector('#button').addEventListener('click', censor);
+// document.querySelector('#button').addEventListener('click', censor);
