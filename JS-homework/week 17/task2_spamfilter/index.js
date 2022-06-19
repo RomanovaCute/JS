bdf3609b59bdce2c0a1999eb50d396e36e77bc6b
RomanovaCute <romanova17romanova@yandex.ru>
2022-06-19 18:16:02 +0500

    week 18 not fix

--- JS-homework/week 17/task2_spamfilter/index.js
+++ JS-homework/week 17/task2_spamfilter/index.js
@@ -2,12 +2,16 @@ const input = document.querySelector('input'),
     button = document.querySelector('#button'),
     list = document.querySelector('#commentsList');
 
-let comments;
+let comments; //объявляем переменную с массивом в глобальной области
+let photos;
 
+// при загрузке страницы вызывается функция, которая сначала достает из локального хранилища заполенный или пустой массив,
+// предварительно превратив массив в виде строки в обычный масссив (parse)
 document.addEventListener('DOMContentLoaded', () => {
     comments = JSON.parse(localStorage.getItem('comments')) || [] ;
     console.log(comments);
     
+    photos = JSON.parse(localStorage.getItem('photo')) || [] ;
     
     // for(let i = 0; i<comments.length; i++){
     //     const box = document.createElement('div');
@@ -21,36 +25,68 @@ document.addEventListener('DOMContentLoaded', () => {
     //     list.append(box)
     // });
 
+    // полученный массив перебирается методом map, и для каждого элемента массива выполняются следующие действия:
     comments.map(comment => {
         const box = document.createElement('div');
         box.innerHTML = comment;
 
         list.append(box);
+
     });
+
+    photos.map(photo => {
+        const box2 = document.createElement('div');
+        box2.innerHTML = `<img src="${valuePhoto}">`
+        list.append(box2);
+    })
 });
 
 
-
-function setData(value){
-    comments.push(value);
-    localStorage.setItem('comments', JSON.stringify(comments));
-    
+// добавление данных комментария в массив, а затем в локальное хранилище с ключом comments, предварительно превратившись в строку (stringify)
+function setData(valueComment){
+    comments.push(valueComment);
+    localStorage.setItem('comments', JSON.stringify(comments)); 
 }
 
+function setPhoto(valuePhoto){
+    photos.push(valuePhoto);
+    localStorage.setItem('photos', JSON.stringify(photos));
+}
+
+
 function postComment(){
-    const value = input.value
+    const valueComment = document.querySelector('#newComment').value
             .replace(/viagra/gi, '***')
             .replace(/xxx/gi, '***');
             
-    setData(value);
+    setData(valueComment);
 
-    
+
     const newListItem = document.createElement('div');
-    newListItem.innerHTML = value;
+    newListItem.innerHTML = valueComment;
+    newListItem.innerHTML = JSON.parse(localStorage.getItem('photos')) || [] ;
     list.append(newListItem);
 }
 
-button.addEventListener('click', postComment)
+function savePhoto(){
+    const valuePhoto = document.querySelector('#userPhoto').value;
+    setPhoto(valuePhoto);
+}
+
+photo_button.addEventListener('click', savePhoto)
+add_button.addEventListener('click', postComment)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
