dc379ae8ec0de73b3c6f7aa4a630b3ec843cfc7f
RomanovaCute <romanova17romanova@yandex.ru>
2022-07-03 16:57:29 +0500

    week 21

---
+++ JS-homework/week 21/task_1/script.js
@@ -0,0 +1,45 @@
+btn.addEventListener('click', getGif);
+let container = document.querySelector('#container');
+
+function getGif() {
+    let search = document.querySelector('#search').value;
+
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=lROTt63xRcpYYrPja4zQWu6YndLhGFZP&q=${search}&limit=25&offset=0&rating=g&lang=en`)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+
+        for(let i = 0; i<5; i++){
+            let imgBox = document.createElement('img')
+            imgBox.setAttribute('id', 'image');
+            imgBox.setAttribute('src', response.data[i].images.downsized.url);
+            container.append(imgBox);
+        }
+})
+        .catch(error => console.log(error))
+
+    container.innerHTML = '';
+    document.querySelector('#search').value = '';
+}
+
+    
+     
+    
+    
+
+
+
+    // url: 'search',
+    // data: {
+    //     api_key: 'lROTt63xRcpYYrPja4zQWu6YndLhGFZP',
+    //     q: dog,
+    //     limit: 25,
+    //     offset: 0,
+    //     raiting: g,
+    //     lang: en
+    // }
+
+
+
+
+
