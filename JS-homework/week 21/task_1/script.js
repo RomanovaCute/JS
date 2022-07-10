cb63d3b701860193cf01af97cd969dfcd99f86a5
RomanovaCute <romanova17romanova@yandex.ru>
2022-07-10 14:40:55 +0500

    week 22 task 1

--- JS-homework/week 21/task_1/script.js
+++ JS-homework/week 21/task_1/script.js
@@ -13,6 +13,7 @@ function getGif() {
             host = 'https://api.giphy.com/v1/gifs/',
             url = 'search';
         
+        // Если запрос введен на английском, а не на русском
         try {
             let errLang = /[a-zA-Z]/gm;
             if(errLang.test(search)){
@@ -23,28 +24,43 @@ function getGif() {
             .then(response => response.json())
             .then(response => {
                 console.log(response);
+        
+        // Если не введен запрос в стркоу поиска
+        try {
+            if(search = ''){
+                throw new Error('Not Found')
+            }
 
             for(let i = 0; i<5; i++){
                 let imgBox = document.createElement('img')
                 imgBox.setAttribute('id', 'image');
                 imgBox.setAttribute('src', response.data[i].images.downsized.url);
                 container.append(imgBox);
-            }      
+            }   
+            
+        } catch (err) {
+            console.log(err.name);
+            console.log(err.message);
+
+            let errSearch = document.createElement('img');
+            errSearch.setAttribute('src', './images/404error.jpg');
+            container.append(errSearch);
+        }
     })  
     
     // .catch(err => console.log(err));
 
     } catch(err) {
             console.log(err.message);
+            console.log(err.name);
 
             let errBox = document.createElement('div');
             let errP = document.createElement('p');
-            errP.append(err.message);
+            errP.append(err.name +':' + ' ' + err.message);
             errBox.append(errP);
             container.append(errBox);
         }  
 
-        
         document.querySelector('#search').value = ''; 
 } 
 
