a73b83459e5851c16b8c1a1a1ffc9fe272bd82d2
RomanovaCute <romanova17romanova@yandex.ru>
2022-07-10 14:17:20 +0500

    week 22 task_1

--- JS-homework/week 21/task_1/script.js
+++ JS-homework/week 21/task_1/script.js
@@ -1,46 +1,51 @@
-btn.addEventListener('click', getGif);
-let container = document.querySelector('#container');
+    btn.addEventListener('click', getGif);
+    let container = document.querySelector('#container');
 
-async function getGif() {
-    let search = document.querySelector('#search').value;
+function getGif() {
+        container.innerHTML = '';
+        let search = document.querySelector('#search').value;
 
-    let apikey = 'lROTt63xRcpYYrPja4zQWu6YndLhGFZP',
-        limit = 5,
-        offset = 0,
-        raiting = 'g',
-        lang = 'ru',
-        host = 'https://api.giphy.com/v1/gifs/',
-        url = 'search';
+        let apikey = 'lROTt63xRcpYYrPja4zQWu6YndLhGFZP',
+            limit = 5,
+            offset = 0,
+            raiting = 'g',
+            lang = 'ru',
+            host = 'https://api.giphy.com/v1/gifs/',
+            url = 'search';
+        
+        try {
+            let errLang = /[a-zA-Z]/gm;
+            if(errLang.test(search)){
+                throw new TypeError('Search in Russian language, please!');
+            } 
+        
+            fetch(`${host}${url}?api_key=${apikey}&q=${search}&limit=${limit}&offset=${offset}&rating=${raiting}&lang=${lang}`)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response);
 
-        fetch(`${host}${url}?api_key=${apikey}&q=${search}&limit=${limit}&offset=${offset}&rating=${raiting}&lang=${lang}`)
-        .then(response => response.json())
-        .then(response => {
-            console.log(response);
+            for(let i = 0; i<5; i++){
+                let imgBox = document.createElement('img')
+                imgBox.setAttribute('id', 'image');
+                imgBox.setAttribute('src', response.data[i].images.downsized.url);
+                container.append(imgBox);
+            }      
+    })  
+    
+    // .catch(err => console.log(err));
 
-        for(let i = 0; i<5; i++){
-            let imgBox = document.createElement('img')
-            imgBox.setAttribute('id', 'image');
-            imgBox.setAttribute('src', response.data[i].images.downsized.url);
-            container.append(imgBox);
-        }
-})
-        .catch(error => console.log(error))
+    } catch(err) {
+            console.log(err.message);
 
-    container.innerHTML = '';
-    document.querySelector('#search').value = '';
-}
+            let errBox = document.createElement('div');
+            let errP = document.createElement('p');
+            errP.append(err.message);
+            errBox.append(errP);
+            container.append(errBox);
+        }  
 
-    // url: 'search',
-    // data: {
-    //     api_key: 'lROTt63xRcpYYrPja4zQWu6YndLhGFZP',
-    //     q: dog,
-    //     limit: 25,
-    //     offset: 0,
-    //     raiting: g,
-    //     lang: en
-    // }
+        
+        document.querySelector('#search').value = ''; 
+} 
 
-
-
-
-
+ 
